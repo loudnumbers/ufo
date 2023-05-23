1d23e39398f2fa8edc8595c0cc2c229988217699
Duncan Geere <radio.edit@gmail.com>
2023-05-23 08:44:10 +0200

    Removing HPF again

--- ufo.lua
+++ ufo.lua
@@ -346,16 +346,6 @@ function add_params()
         end
     )
 
-    -- HPF frequency cutoff control
-    params:add_control('hpf_cutoff', 'high pass filter cutoff',
-        controlspec.new(40, 16000, 'exp', 400, 8500, ''))
-    params:set_action('hpf_cutoff',
-        function(x)
-            engine.hpfCutoff(x)
-            screen_dirty = true
-        end
-    )
-
     ------------------------------
     -- reverb controls
     ------------------------------
